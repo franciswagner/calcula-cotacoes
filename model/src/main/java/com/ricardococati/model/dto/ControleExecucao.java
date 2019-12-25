8000421a27c88e83bf80554d7714ec81ed70d1a5
ricardococati <ricardo.cocati@ifood.com.br>
2019-12-25 09:21:40 -0300

    Inclusão de datas limites nos parâmetros.

--- model/src/main/java/com/ricardococati/model/dto/ControleExecucao.java
+++ model/src/main/java/com/ricardococati/model/dto/ControleExecucao.java
@@ -1,5 +1,6 @@
 package com.ricardococati.model.dto;
 
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +15,24 @@ public class ControleExecucao {
   private Long idControleExecucao;
   private Boolean controleExecucaoAtivo;
   private Boolean calcMediaSimplesDiarioExecutado;
+  private LocalDate calcMediaSimplesDiarioExecutadoDtpreg;
   private Boolean calcMediaSimplesSemanalExecutado;
+  private LocalDate calcMediaSimplesSemanalExecutadoDtpreg;
   private Boolean calcMediaExponencialSemanalExecutado;
+  private LocalDate calcMediaExponencialSemanalExecutadoDtpreg;
   private Boolean calcMediaExponencialDiarioExecutado;
+  private LocalDate calcMediaExponencialDiarioExecutadoDtpreg;
   private Boolean calcMacdDiarioExecutado;
+  private LocalDate calcMacdDiarioExecutadoDtpreg;
   private Boolean calcMacdSemanalExecutado;
+  private LocalDate calcMacdSemanalExecutadoDtpreg;
   private Boolean calcSinalMacdDiarioExecutado;
+  private LocalDate calcSinalMacdDiarioExecutadoDtpreg;
   private Boolean calcSinalMacdSemanalExecutado;
+  private LocalDate calcSinalMacdSemanalExecutadoDtpreg;
   private Boolean calcHistogramaDiarioExecutado;
+  private LocalDate calcHistogramaDiarioExecutadoDtpreg;
   private Boolean calcHistogramaSemanalExecutado;
+  private LocalDate calcHistogramaSemanalExecutadoDtpreg;
 
 }
--- repository/src/main/java/com/ricardococati/repository/dao/sqlutil/ControleExecucaoSQLUtil.java
+++ repository/src/main/java/com/ricardococati/repository/dao/sqlutil/ControleExecucaoSQLUtil.java
@@ -37,11 +37,16 @@ public class ControleExecucaoSQLUtil {
   public String getUpdateDiario() {
     final SQLAppender sql = new SQLAppender(100);
     sql.appendSQL(" update controle_execucao set  ");
-    sql.appendSQL("   calc_media_movel_diario_executado = :calcMediaMovelDiarioExecutada,  ");
-    sql.appendSQL("   calc_media_exponecial_diario_executado = :calcMediaExponecialDiarioExecutada,  ");
-    sql.appendSQL("   calc_macd_diario_executado = :calcMacdDiarioExecutada,  ");
-    sql.appendSQL("   calc_sinal_macd_diario_executado = :calcSinalMacdDiarioExecutada,  ");
-    sql.appendSQL("   calc_histograma_diario_executado = :calcHistogramaDiarioExecutada  ");
+    sql.appendSQL("     calc_media_movel_diario_executado = :calcMediaMovelDiarioExecutado, ");
+    sql.appendSQL("     calc_media_movel_diario_executado_dtexec = :calcMediaMovelDiarioExecutadoDtexec, ");
+    sql.appendSQL("     calc_media_exponecial_diario_executado = :calcMovelExponecialDiarioExecutado, ");
+    sql.appendSQL("     calc_media_exponecial_diario_executado_dtexec = :calcMovelExponecialDiarioExecutadoDtexec, ");
+    sql.appendSQL("     calc_macd_diario_executado = :calcMacdDiarioExecutado, ");
+    sql.appendSQL("     calc_macd_diario_executado_dtexec = :calcMacdDiarioExecutadoDtexec, ");
+    sql.appendSQL("     calc_sinal_macd_diario_executado = :calcSinalMacdDiarioExecutado, ");
+    sql.appendSQL("     calc_sinal_macd_diario_executado_dtexec = :calcSinalMacdDiarioExecutadoDtexec, ");
+    sql.appendSQL("     calc_histograma_diario_executado = :calcHistogramaDiarioExecutado, ");
+    sql.appendSQL("     calc_histograma_diario_executado_dtexec = :calcHistogramaDiarioExecutadoDtexec ");
     sql.appendSQL(" where id_controle_execucao = :idControleExecucao ");
     return sql.getAppendSQLSemQuebra().toString();
   }
@@ -49,22 +54,32 @@ public class ControleExecucaoSQLUtil {
   public MapSqlParameterSource toParametersUpdateDiario(
       final ControleExecucao controleExecucao) {
     return new MapSqlParameterSource()
-        .addValue("calcMediaMovelDiarioExecutada", controleExecucao.getCalcMediaSimplesDiarioExecutado())
-        .addValue("calcMediaExponecialDiarioExecutada", controleExecucao.getCalcMediaExponencialDiarioExecutado())
-        .addValue("calcMacdDiarioExecutada", controleExecucao.getCalcMacdDiarioExecutado())
-        .addValue("calcSinalMacdDiarioExecutada", controleExecucao.getCalcSinalMacdDiarioExecutado())
-        .addValue("calcHistogramaDiarioExecutada", controleExecucao.getCalcHistogramaDiarioExecutado())
+        .addValue("calcMediaMovelDiarioExecutado", controleExecucao.getCalcMediaSimplesDiarioExecutado())
+        .addValue("calcMediaMovelDiarioExecutadoDtexec", controleExecucao.getCalcMediaSimplesDiarioExecutadoDtpreg())
+        .addValue("calcMovelExponecialDiarioExecutado", controleExecucao.getCalcMediaExponencialDiarioExecutado())
+        .addValue("calcMovelExponecialDiarioExecutadoDtexec", controleExecucao.getCalcMediaExponencialDiarioExecutadoDtpreg())
+        .addValue("calcMacdDiarioExecutado", controleExecucao.getCalcMacdDiarioExecutado())
+        .addValue("calcMacdDiarioExecutadoDtexec", controleExecucao.getCalcMacdDiarioExecutadoDtpreg())
+        .addValue("calcSinalMacdDiarioExecutado", controleExecucao.getCalcSinalMacdDiarioExecutado())
+        .addValue("calcSinalMacdDiarioExecutadoDtexec", controleExecucao.getCalcSinalMacdDiarioExecutadoDtpreg())
+        .addValue("calcHistogramaDiarioExecutado", controleExecucao.getCalcHistogramaDiarioExecutado())
+        .addValue("calcHistogramaDiarioExecutadoDtexec", controleExecucao.getCalcHistogramaDiarioExecutadoDtpreg())
         .addValue("idControleExecucao", controleExecucao.getIdControleExecucao());
   }
 
   public String getUpdateSemanal() {
     final SQLAppender sql = new SQLAppender(100);
     sql.appendSQL(" update controle_execucao set  ");
-    sql.appendSQL("   calc_media_movel_semanal_executado = :calcMediaMovelSemanalExecutada,  ");
-    sql.appendSQL("   calc_media_exponecial_semanal_executado = :calcMediaExponecialSemanalExecutada,  ");
-    sql.appendSQL("   calc_macd_semanal_executado = :calcMacdSemanalExecutada,  ");
-    sql.appendSQL("   calc_sinal_macd_semanal_executado = :calcSinalMacdSemanalExecutada,  ");
-    sql.appendSQL("   calc_histograma_semanal_executado = :calcHistogramaSemanalExecutada  ");
+    sql.appendSQL("     calc_media_movel_semanal_executado = :calcMediaMovelSemanalExecutado, ");
+    sql.appendSQL("     calc_media_movel_semanal_executado_dtexec = :calcMediaMovelSemanalExecutadoDtexec, ");
+    sql.appendSQL("     calc_media_exponecial_semanal_executado = :calcMovelExponecialSemanalExecutado, ");
+    sql.appendSQL("     calc_media_exponecial_semanal_executado_dtexec = :calcMovelExponecialSemanalExecutadoDtexec, ");
+    sql.appendSQL("     calc_macd_semanal_executado = :calcMacdSemanalExecutado, ");
+    sql.appendSQL("     calc_macd_semanal_executado_dtexec = :calcMacdSemanalExecutadoDtexec, ");
+    sql.appendSQL("     calc_sinal_macd_semanal_executado = :calcSinalMacdSemanalExecutado, ");
+    sql.appendSQL("     calc_sinal_macd_semanal_executado_dtexec = :calcSinalMacdSemanalExecutadoDtexec, ");
+    sql.appendSQL("     calc_histograma_semanal_executado = :calcHistogramaSemanalExecutado, ");
+    sql.appendSQL("     calc_histograma_semanal_executado_dtexec = :calcHistogramaSemanalExecutadoDtexec ");
     sql.appendSQL(" where id_controle_execucao = :idControleExecucao ");
     return sql.getAppendSQLSemQuebra().toString();
   }
@@ -72,11 +87,16 @@ public class ControleExecucaoSQLUtil {
   public MapSqlParameterSource toParametersUpdateSemanal(
       final ControleExecucao controleExecucao) {
     return new MapSqlParameterSource()
-        .addValue("calcMediaMovelSemanalExecutada", controleExecucao.getCalcMediaSimplesSemanalExecutado())
-        .addValue("calcMediaExponecialSemanalExecutada", controleExecucao.getCalcMediaExponencialSemanalExecutado())
-        .addValue("calcMacdSemanalExecutada", controleExecucao.getCalcMacdSemanalExecutado())
-        .addValue("calcSinalMacdSemanalExecutada", controleExecucao.getCalcSinalMacdSemanalExecutado())
-        .addValue("calcHistogramaSemanalExecutada", controleExecucao.getCalcHistogramaSemanalExecutado())
+        .addValue("calcMediaMovelSemanalExecutado", controleExecucao.getCalcMediaSimplesSemanalExecutado())
+        .addValue("calcMediaMovelSemanalExecutadoDtexec", controleExecucao.getCalcMediaSimplesSemanalExecutadoDtpreg())
+        .addValue("calcMovelExponecialSemanalExecutado", controleExecucao.getCalcMediaExponencialSemanalExecutado())
+        .addValue("calcMovelExponecialSemanalExecutadoDtexec", controleExecucao.getCalcMediaExponencialSemanalExecutadoDtpreg())
+        .addValue("calcMacdSemanalExecutado", controleExecucao.getCalcMacdSemanalExecutado())
+        .addValue("calcMacdSemanalExecutadoDtexec", controleExecucao.getCalcMacdSemanalExecutadoDtpreg())
+        .addValue("calcSinalMacdSemanalExecutado", controleExecucao.getCalcSinalMacdSemanalExecutado())
+        .addValue("calcSinalMacdSemanalExecutadoDtexec", controleExecucao.getCalcSinalMacdSemanalExecutadoDtpreg())
+        .addValue("calcHistogramaSemanalExecutado", controleExecucao.getCalcHistogramaSemanalExecutado())
+        .addValue("calcHistogramaSemanalExecutadoDtexec", controleExecucao.getCalcHistogramaSemanalExecutadoDtpreg())
         .addValue("idControleExecucao", controleExecucao.getIdControleExecucao());
   }
 
