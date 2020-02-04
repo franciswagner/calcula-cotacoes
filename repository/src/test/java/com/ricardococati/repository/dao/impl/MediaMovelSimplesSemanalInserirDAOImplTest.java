610f28108507e70a25e82969a88669f20f7b04c8
ricardococati <ricardo.cocati@ifood.com.br>
2020-02-04 17:25:37 -0300

    Incluindo testes para MediaMovelSimplesSemanalInserirDAOImpl

---
+++ repository/src/test/java/com/ricardococati/repository/dao/impl/MediaMovelSimplesSemanalInserirDAOImplTest.java
@@ -0,0 +1,125 @@
+package com.ricardococati.repository.dao.impl;
+
+import static br.com.six2six.fixturefactory.Fixture.from;
+import static com.ricardococati.repository.dao.templates.MediaMovelSimplesSemanalTemplateLoader.MEDIA_MOVEL_SIMPLES_SEMANAL_VALID_001;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+import com.ricardococati.model.dto.MediaMovelSimplesSemanal;
+import com.ricardococati.repository.dao.BaseJdbcTest;
+import com.ricardococati.repository.dao.sqlutil.MediaMovelSimplesSemanalSQLUtil;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MediaMovelSimplesSemanalInserirDAOImplTest extends BaseJdbcTest {
+
+  @InjectMocks
+  private MediaMovelSimplesSemanalInserirDAOImpl target;
+  @Mock
+  private GeraSequenciaDAOImpl genericDAO;
+  @Mock
+  private MediaMovelSimplesSemanalSQLUtil sqlUtil;
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() throws Exception {
+    FixtureFactoryLoader.loadTemplates("com.ricardococati.repository.dao.templates");
+    target = new MediaMovelSimplesSemanalInserirDAOImpl(
+        getNamedParameterJdbcTemplate(),
+        genericDAO,
+        sqlUtil
+    );
+  }
+
+  @Test
+  public void incluirMediaMovelSimples() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    MediaMovelSimplesSemanal dto = buildMMSSemanal();
+    //when
+    Boolean retorno = target.incluirMediaMovelSimples(dto);
+    //then
+    assertTrue(retorno);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialSemanalNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    MediaMovelSimplesSemanal dto = null;
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de MEDIA_MOVEL_SIMPLES_SEMANAL"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirMediaMovelSimples(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialDtPregNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    MediaMovelSimplesSemanal dto = buildMMSSemanal();
+    dto.setDtpregini(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de MEDIA_MOVEL_SIMPLES_SEMANAL"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirMediaMovelSimples(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    MediaMovelSimplesSemanal dto = buildMMSSemanal();
+    dto.setMediaMovelSimples(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de MEDIA_MOVEL_SIMPLES_SEMANAL"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirMediaMovelSimples(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialCodNegNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    MediaMovelSimplesSemanal dto = buildMMSSemanal();
+    dto.getMediaMovelSimples().setCodneg(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de MEDIA_MOVEL_SIMPLES_SEMANAL"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirMediaMovelSimples(dto);
+  }
+
+  private MediaMovelSimplesSemanal buildMMSSemanal(){
+    return from(MediaMovelSimplesSemanal.class)
+        .gimme(MEDIA_MOVEL_SIMPLES_SEMANAL_VALID_001);
+  }
+}
