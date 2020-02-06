3a58123997a3b5f6d94d944c53714cbd7fc2ee7c
ricardococati <ricardo.cocati@ifood.com.br>
2020-02-06 00:06:31 -0300

    Incluindo testes de recomendacao diario e semanal

---
+++ repository/src/test/java/com/ricardococati/repository/dao/impl/RecomendacaoDiarioInserirDAOImplTest.java
@@ -0,0 +1,126 @@
+package com.ricardococati.repository.dao.impl;
+
+import static br.com.six2six.fixturefactory.Fixture.from;
+import static com.ricardococati.repository.dao.templates.RecomendacaoDiarioTemplateLoader.RECOMENDACAO_DIARIO_VALID_001;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+import com.ricardococati.model.dto.RecomendacaoDiario;
+import com.ricardococati.repository.dao.config.BaseJdbcTest;
+import com.ricardococati.repository.dao.sqlutil.RecomendacaoDiarioInserirSQLUtil;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class RecomendacaoDiarioInserirDAOImplTest extends BaseJdbcTest {
+
+  @InjectMocks
+  private RecomendacaoDiarioInserirDAOImpl target;
+  @Mock
+  private GeraSequenciaDAOImpl genericDAO;
+  @Mock
+  private RecomendacaoDiarioInserirSQLUtil sqlUtil;
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() throws Exception {
+    FixtureFactoryLoader.loadTemplates("com.ricardococati.repository.dao.templates");
+    target = new RecomendacaoDiarioInserirDAOImpl(
+        getNamedParameterJdbcTemplate(),
+        genericDAO,
+        sqlUtil
+    );
+  }
+
+  @Test
+  public void incluirRecomendacao() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    RecomendacaoDiario dto = buildRecomendacao();
+    //when
+    Boolean retorno = target.incluirRecomendacao(dto);
+    //then
+    assertTrue(retorno);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialSemanalNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    RecomendacaoDiario dto = null;
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de RECOMENDACAO_DIARIO"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirRecomendacao(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialDtPregNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    RecomendacaoDiario dto = buildRecomendacao();
+    dto.setDtpreg(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de RECOMENDACAO_DIARIO"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirRecomendacao(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    RecomendacaoDiario dto = buildRecomendacao();
+    dto.setRecomendacao(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de RECOMENDACAO_DIARIO"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirRecomendacao(dto);
+  }
+
+  @Test
+  public void incluirMediaMovelExponencialCodNegNull() {
+    //given
+    when(genericDAO.getSequence(any())).thenReturn(1);
+    when(sqlUtil.getInsert()).thenCallRealMethod();
+    when(sqlUtil.toParameters(any())).thenCallRealMethod();
+    RecomendacaoDiario dto = buildRecomendacao();
+    dto.getRecomendacao().setCodneg(null);
+    this.thrown.expectMessage(
+        "Violação de integridade na inserção de RECOMENDACAO_DIARIO"
+    );
+    this.thrown.expect(DataIntegrityViolationException.class);
+    //when
+    Boolean retorno = target.incluirRecomendacao(dto);
+  }
+
+  private RecomendacaoDiario buildRecomendacao() {
+    return from(RecomendacaoDiario.class)
+        .gimme(RECOMENDACAO_DIARIO_VALID_001);
+  }
+
+}
