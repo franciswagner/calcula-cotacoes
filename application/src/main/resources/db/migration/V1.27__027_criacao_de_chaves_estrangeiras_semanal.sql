f1140bafe8a1233e30312b1c039575f9a5898f7b
ricardococati <ricardo.cocati@ifood.com.br>
2020-03-10 13:18:34 -0300

    Criando chaves estrangeiras em todas as tabelas.

--- application/src/main/resources/db/migration/V1.27__027_criacao_de_chaves_estrangeiras_semanal.sql
+++ application/src/main/resources/db/migration/V1.27__027_criacao_de_chaves_estrangeiras_semanal.sql
@@ -1,10 +1,15 @@
---ALTER TABLE media_movel_simples_semanal DROP CONSTRAINT fk_media_movel_simples_semanal_candlestick;
---ALTER TABLE media_movel_exponencial_semanal DROP CONSTRAINT fk_media_movel_exponencial_semanal_candlestick;
---ALTER TABLE macd_semanal DROP CONSTRAINT fk_macd_semanal_candlestick;
---ALTER TABLE sinal_macd_semanal DROP CONSTRAINT fk_sinal_macd_semanal_candlestick;
---ALTER TABLE histograma_semanal DROP CONSTRAINT fk_histograma_semanal_candlestick;
---ALTER TABLE histograma_semanal DROP CONSTRAINT fk_histograma_semanal_candlestick;
---ALTER TABLE recomendacao_semanal DROP CONSTRAINT fk_recomendacao_semanal_candlestick;
+ALTER TABLE candlestick_semanal DROP CONSTRAINT IF EXISTS fk_candlestick_semanal_candlestick;
+ALTER TABLE media_movel_simples_semanal DROP CONSTRAINT IF EXISTS fk_media_movel_simples_semanal_candlestick;
+ALTER TABLE media_movel_exponencial_semanal DROP CONSTRAINT IF EXISTS fk_media_movel_exponencial_semanal_candlestick;
+ALTER TABLE macd_semanal DROP CONSTRAINT IF EXISTS fk_macd_semanal_candlestick;
+ALTER TABLE sinal_macd_semanal DROP CONSTRAINT IF EXISTS fk_sinal_macd_semanal_candlestick;
+ALTER TABLE histograma_semanal DROP CONSTRAINT IF EXISTS fk_histograma_semanal_candlestick;
+ALTER TABLE histograma_semanal DROP CONSTRAINT IF EXISTS fk_histograma_semanal_candlestick;
+ALTER TABLE recomendacao_semanal DROP CONSTRAINT IF EXISTS fk_recomendacao_semanal_candlestick;
+
+ALTER TABLE candlestick_semanal
+ADD CONSTRAINT fk_candlestick_semanal_candlestick
+FOREIGN KEY (codneg, dtpregini) REFERENCES candlestick_diario (codneg, dtpreg);
 
 ALTER TABLE media_movel_simples_semanal
 ADD CONSTRAINT fk_media_movel_simples_semanal_candlestick
