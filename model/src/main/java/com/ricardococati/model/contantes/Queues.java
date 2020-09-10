7198a4a3fdfa0cb57ee10471a03f22e99852f9ba
Ricardo Cocati <ricardo.cocati@ifood.com.br>
2020-09-10 01:07:33 -0300

    Inclundo config do RabbitMQ no sistema

--- model/src/main/java/com/ricardococati/model/contantes/Queues.java
+++ model/src/main/java/com/ricardococati/model/contantes/Queues.java
@@ -2,7 +2,7 @@ package com.ricardococati.model.contantes;
 
 public final class Queues {
 
-  public static final String ORDER_INPUT_RECOMENDA_DIARIO = "recomendacao.diario.calcula.input";
-  public static final String ORDER_INPUT_RECOMENDA_SEMANAL = "recomendacao.semanal.calcula.input";
+  public static final String RECOMENDA_INPUT_DIARIO = "recomendacao.diario.calcula.input";
+  public static final String RECOMENDA_INPUT_SEMANAL = "recomendacao.semanal.calcula.input";
 
 }
