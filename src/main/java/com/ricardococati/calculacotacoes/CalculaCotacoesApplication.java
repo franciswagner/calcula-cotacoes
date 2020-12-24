762570e3dc785b8459b039486c2a382e82330bfe
ricardococati <ricardo.cocati@gmail.com>
2020-12-23 23:23:11 -0300

    Migração para o spring 2.4 e remoção de modulos do spring

---
+++ src/main/java/com/ricardococati/calculacotacoes/CalculaCotacoesApplication.java
@@ -0,0 +1,13 @@
+package com.ricardococati.calculacotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaCotacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaCotacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardococati/calculacotacoes/CalculaCotacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardococati.calculacotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculaCotacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
